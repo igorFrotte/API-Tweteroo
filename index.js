23bbfa4512676ec111bbc9f0a46e470f0dd618b8
igorFrotte <igorfrotte@gmail.com>
2022-08-27 09:20:40 -0300

    Post tweets ready

--- index.js
+++ index.js
@@ -14,4 +14,10 @@ app.post('/sign-up', (req, res) => {
   res.send("OK");
 });
 
+app.post('/tweets', (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
+  res.send("OK");
+});
+
 app.listen(5000);
